b81a61e948a41aaf58d8ef4cb68e85961efb78e4
Skip Khamvongsa <skipk95@gmail.com>
2020-03-04 21:18:01 -0700

    joining forces

--- javascript.js
+++ javascript.js
@@ -1,15 +1,23 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=7UZtOVuuKSWAGSyxeMuY5Zjxbo39d6WE" 
- 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(r) {
-    console.log(r);
-    console.log(r.response.docs[0].headline.main);
-    console.log(r.response.docs[0].pub_date);
-    console.log(r.response.docs[0].byline.original);
-    console.log(r.response.docs[0].word_count);
-    console.log(r.response.docs[0].snippet)
-    var title = $("<h1>");
+$(document).ready(function() { 
+    var searching = "article";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ searching +"&api-key=7UZtOVuuKSWAGSyxeMuY5Zjxbo39d6WE" 
 
+    $("#search-btn").on("click", function() {
+        searching = $("#term").val()
+        // alert($("#inputGroupSelect01").val())
+        console.log(searching);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(r) {
+            console.log(r);
+            console.log(r.response.docs[0].headline.main);
+            console.log(r.response.docs[0].pub_date);
+            console.log(r.response.docs[0].byline.original);
+            console.log(r.response.docs[0].word_count);
+            console.log(r.response.docs[0].snippet)
+            var title = $("<h1>");
+        })
+    })
 });
